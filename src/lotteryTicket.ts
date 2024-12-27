78525161f1cfed80d9450706f5e8479196ec4ce6
OsmanBytyqi <obytyqi2001@gmail.com>
2024-12-27 10:37:14 +0100

    Update lotteryTicket.ts

--- src/lotteryTicket.ts
+++ src/lotteryTicket.ts
@@ -37,9 +37,3 @@ console.log(expectedMax([10, 10, 10, 10, 10, 10, 10], 3)); // Output: 10.0
 console.log(expectedMax([10, 50, 40, 20, 30], 5)); // Output: 50.0
 console.log(expectedMax([11, 12, 13, 14, 15, 16, 18], 1)); // Output: 14.142857142857142
 
-
-// Profa ka thane mos i dergoni me komente
-
-
-//Time Complexity: approximately ( O(n^2) ) in the worst case
-//Space Complexity: ( O(n) )
